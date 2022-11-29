96b8bcf04cb587f9f19424213f03a11fe006bfc8
Quartz <skyu.dev@gmail.com>
2022-11-29 14:32:54 +0900

    chore: use airbnb config and add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    quotes: ["off", "single"],
+    "class-methods-use-this": "off",
+    "arrow-body-style": "off",
+    "no-use-before-define": ["error", { variables: false }],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/react-in-jsx-scope": "off",
+    // "max-depth": ["error", 2],
+    // "max-lines-per-function": ["error", 10],
+    // "max-params": ["error", 3],
+  },
+  plugins: ["import", "react"],
+};
